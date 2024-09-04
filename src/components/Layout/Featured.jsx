313f1511b4317e2ed1b6c37dfc26feeedfdbeafd
Dilysnguyen <91045422+dilys23@users.noreply.github.com>
2024-09-04 15:15:35 +0700

    create navbar and mainpage

---
+++ src/components/Layout/Featured.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Fetured() {
+  return <div />;
+}
+
+export default Fetured;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
+      colors: {
+        primary: "#3498db",
+        light: "#3498db",
+        gray: "#3498db",
+        lightGray : "#3498db",
+        orange: "#3498db",    
+
+      }
+    },
+  },
+  plugins: [],
+}
+
