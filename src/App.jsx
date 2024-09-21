1ad0c251c75e4f4e4a717226350b6d60cc5d3438
bachduong42 <bachduongvan0402@gmail.com>
2024-09-21 12:53:17 +0700

    fix toast

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { publicRoutes } from "./router"
 import { DefaultLayout } from "./components/Layout"
 import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
 import Footer from "./components/Footer/Footer"
 import Home from "./pages/Home"
 import Register from "./components/Layout/Register"
@@ -15,14 +14,7 @@ function App() {
 
   return (
     <>
-      <ToastContainer className="justify-center" />
-      {/* <Navbar />
-      <Hero />
-      <Fetured />
-      <Packages />
-      <Extension />
-      {/* <Discover/> */}
-      {/* <Home></Home> */}
+
       <Router>
         <div className="App">
           <Routes>
--- src/components/Layout/DefaultLayout/DefaultLayout.jsx
+++ src/components/Layout/DefaultLayout/DefaultLayout.jsx
@@ -1,17 +1,21 @@
 import PropTypes from 'prop-types';
 import Navbar from "../../Header/Navbar"
 import Sidebar from "../../Layout/components/Sidebar"
+import { ToastContainer } from 'react-toastify';
 function DefaultLayout({ children }) {
     return (
-        <div className="bg-white">
-            <Navbar></Navbar>
-            <div className="mx-6 bg-bgGray ">
-                <div className='flex md:gap-x-15 lg:gap-x-28 gap-x-0 w-full'>
-                    <Sidebar></Sidebar>
-                    <div className="w-5/6">{children}</div>
+        <>
+
+            <div className="bg-white">
+                <Navbar></Navbar>
+                <div className="mx-6 bg-bgGray ">
+                    <div className='flex md:gap-x-15 lg:gap-x-28 gap-x-0 w-full'>
+                        <Sidebar></Sidebar>
+                        <div className="w-5/6">{children}</div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
