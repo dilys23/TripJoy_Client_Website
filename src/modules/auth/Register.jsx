6441074c04bd6dbf82550e785aee638a9809f058
Dilysnguyen <lenguyen.231203@gmail.com>
2024-09-29 09:30:37 +0700

    register success

--- src/modules/auth/Register.jsx
+++ src/modules/auth/Register.jsx
@@ -3,23 +3,57 @@ import axios from "axios";
 import InputButton from "../../components/Input/InputButton";
 import InputField from "../../components/Input/InputField";
 import { useAuthStore } from "../../services/authUser";
+import { sendOTP , register1} from "../../services/authAccount";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 function Register({ onClose }) {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phoneNumber, setphoneNumber] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    otp: "",
+    phoneNumber: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
   const { register, isRegister, user } = useAuthStore();
-  // const navigate = useNavigate();
 
-  const handleRegister = (e) => {
+  const handleSendOTP = async (e) => {
     e.preventDefault();
-    register({ email, phoneNumber, name, password, confirmPassword });
-    console.log(isRegister);
+    console.log({ email: formData.email });
+    try {
+      const result = await sendOTP({ email: formData.email });
+      console.log(result);
+      console.log("gửi otp đến email thành công ", result);
+    } catch (error) {
+      console.error("Lỗi khi gửi OTP:", error.message);
+    }
+  };
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      await registqer1({
+        email: formData.email,
+        phoneNumber: formData.phoneNumber,
+        name: formData.name,
+        password: formData.password,
+        confirmPassword: formData.confirmPassword,
+        otp: formData.otp,
+      });
+      console.log("Đăng kí thành công  ", result);
+    } catch (error) {
+      console.error("Lỗi khi gửi thông tin:", error.message);
+    }
   };
   const validateName = (value) => {
     if (!value) return "Tên không được để trống ";
-     return "";
+    return "";
   };
   const validateEmail = (value) => {
     if (!value) return "Email không được để trống ";
@@ -41,6 +75,11 @@ function Register({ onClose }) {
   const validateConfirmPassword = (value) => {
     if (!value) return "Xác nhận mật khẩu không được để trống";
     if (value !== password) return "Mật khẩu không trùng khớp";
+    return "";
+  };
+  const validateOTP = (value) => {
+    if (!value) return "OTP không được để trống";
+    if (value.length !== 6) return "OTP phải đủ 6 số";
     return "";
   };
   return (
@@ -114,12 +153,11 @@ function Register({ onClose }) {
             </div>
 
             <form className="w-full px-3" onSubmit={handleRegister}>
-              
               <InputField
                 name="name"
                 type="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+                value={formData.name}
+                onChange={handleInputChange}
                 validate={validateName}
                 placeholder="Tên"
                 required={true}
@@ -127,38 +165,66 @@ function Register({ onClose }) {
               <InputField
                 name="email"
                 type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                value={formData.email}
+                onChange={handleInputChange}
                 validate={validateEmail}
                 placeholder="Email"
                 required={true}
               />
-              <InputButton />
+              {/* <InputButton /> */}
+              <form
+                className="mx-auto flex max-w-lg items-center gap-5"
+                onSubmit={handleSendOTP}
+              >
+                <InputField
+                  name="otp"
+                  type="text"
+                  value={formData.otp}
+                  onChange={handleInputChange}
+                  placeholder="OTP"
+                  required={true}
+                  validate={validateOTP}
+                  className="w-full"
+                  inputClassName="h-[40px] w-full rounded-[5px] border border-[#fffff] bg-[#f5f6f7] px-3 shadow focus:border-blue-400 focus:outline-none" // Class cho input
+                />
+
+                <button
+                  className="mb-4 inline-flex h-[40px] w-[40%] items-center rounded-[5px] border border-blue-700 bg-blue-700 px-5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  type="submit"
+                  onClick={handleSendOTP}
+                >
+                  <FontAwesomeIcon
+                    icon={faPaperPlane}
+                    size="1x"
+                    className="ml-[-2px] px-2"
+                    color="white"
+                  />
+                  Gửi OTP
+                </button>
+              </form>
               <InputField
                 name="phoneNumber"
                 type="text"
-                value={phoneNumber}
-                onChange={(e) => setphoneNumber(e.target.value)}
+                value={formData.phoneNumber}
+                onChange={handleInputChange}
                 validate={validatePhoneNumber}
-                // errorMessage="Mat khau khong hop le"
                 placeholder="Số điện thoại"
                 required={true}
               />
               <InputField
                 name="password"
                 type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                value={formData.password}
+                onChange={handleInputChange}
                 validate={validatePassword}
-                // errorMessage="Mat khau khong hop le"
                 placeholder="Mật khẩu"
                 required={true}
               />
               <InputField
-                name="validateConfirmPassword"
+                name="confirmPassword"
                 type="password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
+                value={formData.confirmPassword}
+                onChange={handleInputChange}
                 validate={validateConfirmPassword}
                 placeholder="Xác nhận mật khẩu"
                 required={true}
