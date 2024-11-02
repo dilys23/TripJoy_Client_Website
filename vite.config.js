291228e8ec0aaa2b3c691a706aeb386cc189c65f
Dilysnguyen <91045422+dilys23@users.noreply.github.com>
2024-11-02 08:58:45 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,9 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    rollupOptions: {
+      external: ['leaflet-geosearch'], // Loại bỏ module khỏi quá trình build
+    },
+  },
 });
